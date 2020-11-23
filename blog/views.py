00e5d09145034ed50c747d6186092cedb345edd4
alan <alankritverma45@gmail.com>
2020-11-23 11:39:14 +0530

    Added password reset feature and resolved profile image updation issue

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
 from django.views.generic import (ListView,
@@ -8,6 +8,7 @@ from django.views.generic import (ListView,
                                   DeleteView
                                   )
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.models import User
 # Create your views here.
 posts = [
      {
@@ -27,6 +28,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html'
     context_object_name = 'post_list'
     ordering = ['-date_posted']     
+    paginate_by = 3
 
 class PostDetailView(DetailView):
     model = Post    
@@ -64,6 +66,21 @@ class PostDeleteView(LoginRequiredMixin ,UserPassesTestMixin,DeleteView):
         if self.request.user == post.author:
             return True
         return False              
+
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    context_object_name = 'posts'
+    # ordering = '-date_posted'
+    paginate_by = 5
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username = self.kwargs.get('username'))
+        # print(Post.objects.filter(author = user).order_by('-date_posted'))
+        return Post.objects.filter(author = user).order_by('-date_posted')
+
+
 
 def about(request):
     return render(request,"blog/about.html",{"title": "About"})
